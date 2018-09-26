4047b5d1c59b155021eec83b064d97dfa059acb6
Metal Star <metalstar013@gmail.com>
2018-09-26 17:46:44 +0530

    Minor changes

--- app/src/main/java/com/automator/bruyant/LightStatusFragment.kt
+++ app/src/main/java/com/automator/bruyant/LightStatusFragment.kt
@@ -5,17 +5,34 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
+import kotlinx.android.synthetic.main.fragment_light_status.*
 
 /**
  * Created by Kishan P Rao on 26/09/18.
  */
-class LightStatusFragment: Fragment() {
-	lateinit var mqttController: MqttController
-	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-		return inflater.inflate(R.layout.fragment_light_status, container, false)
-	}
-	
-	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-		super.onViewCreated(view, savedInstanceState)
-	}
+class LightStatusFragment : Fragment(), MqttController.StatusCallback {
+    private var previousState = false
+
+    override fun onLightChange(on: Boolean) {
+        toggleLight.isChecked = on
+    }
+
+    lateinit var mqttController: MqttController
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_light_status, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mqttController.callback = this
+        toggleLight.setOnCheckedChangeListener(object : CompoundButton.OnCheckedChangeListener {
+            override fun onCheckedChanged(p0: CompoundButton?, p1: Boolean) {
+                if (previousState != p1) {
+                    mqttController.changeLight(p1)
+                    previousState = p1
+                }
+            }
+        })
+    }
 }
